ae44188ed70df676a4e61823328b15901f081272
Gerben van Rijswijk <gvrijswijk@gmail.com>
2017-12-02 09:59:11 +0100

    Solved day 02 puzzle 02 (total: 04)

---
+++ 2017/day02/puzzle04.py
@@ -0,0 +1,17 @@
+# Advent of Code 2017
+# Day 02; Puzzle 03
+
+solution = 0
+
+# Open the input file
+with open('input.txt') as input:
+    x = input.read().strip()
+
+for line in x.split('\n'):
+    values = list(map(float, line.split('\t')))
+    for i in range(len(values)):
+        for j in range(len(values)):
+            if (i != j) and (values[i]/values[j] == int(values[i]/values[j])):
+                solution += int(values[i]/values[j])
+
+print(solution)
