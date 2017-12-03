14c9ac16e055934eb72e350397ae0929457cff4d
Gerben van Rijswijk <gvrijswijk@gmail.com>
2017-12-03 14:21:05 +0100

    Solved day 03 puzzle 01 (total: 05)

---
+++ 2017/day03/puzzle05.py
@@ -0,0 +1,24 @@
+# Advent of Code 2017
+# Day 03; Puzzle 05
+
+def moves(value):
+
+    total = value
+    count = 0
+    step = 0
+
+    while total > step:
+        count = count + 1
+        step = int(count/2)
+        total = total - max(1, step)
+
+        if total == 0:
+            steps = step
+        elif total > step/2:
+            steps = total
+        else:
+            steps = step - total
+
+    print(steps)
+
+moves(312051)
