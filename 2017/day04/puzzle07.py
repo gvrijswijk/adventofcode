82c8af6b6a4ba441afcc96ae6c47d834437b98e5
Gerben van Rijswijk <gvrijswijk@gmail.com>
2017-12-04 08:38:42 +0100

    Solved day 04 puzzle 01 (total: 07)

---
+++ 2017/day04/puzzle07.py
@@ -0,0 +1,15 @@
+# Advent of Code 2017
+# Day 04; Puzzle 07
+
+count = 0
+
+with open('input.txt') as input:
+    x = input.read().strip()
+
+for line in x.split('\n'):
+    if line.islower and line.isalpha:
+        words = line.split(' ')
+        if len(words) == len(set(words)):
+            count += 1
+
+print(count)
