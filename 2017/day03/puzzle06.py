71a9b51b1b4811a289bebf0a7d325860e10ce8a9
Gerben van Rijswijk <gvrijswijk@gmail.com>
2017-12-09 23:24:29 +0100

    First attempt at solving puzzle 06

--- 2017/day03/puzzle06.py
+++ 2017/day03/puzzle06.py
@@ -1,5 +1,43 @@
-#
-#
-# Solved this in Excel. 
-#
-#
+# Advent of Code 2017
+# Day 03; Puzzle 06
+
+t = 1
+x = 0
+y = 0
+v = 0
+
+d = {
+    '0,0': 1
+}
+
+while v < 277678:
+
+    s = int(t / 2)
+
+    for i in range(s):
+        if s % 2 == 0:
+            if s == t / 2:
+                x = x - 1
+            else:
+                y = y -1
+        else:
+            if s == t / 2:
+                x = x + 1
+            else:
+                y = y + 1
+
+        l = str(x)+","+str(y)
+
+        w = 0
+
+        for i in range(x - 1,x + 2, 1):
+            for j in range(y - 1,y + 2, 1):
+                m = str(i)+","+str(j)
+                w += d.get(m, 0)
+        v = max(1,w)
+
+        d[l] = v
+
+    t += 1
+
+print(v)
