1576f7daacbeae2541c2c3db45d4f5394a8672bc
Gerben van Rijswijk <gvrijswijk@gmail.com>
2017-12-16 11:31:13 +0100

    Solved day 16 part 2 too

--- 2017/day16/16_permutation_promenade.py
+++ 2017/day16/16_permutation_promenade.py
@@ -21,11 +21,12 @@ if __name__ == "__main__":
         #input = ['s1', 'x3/4', 'pe/b'] #Test
 
 line = 'abcdefghijklmnop'
+times = 1000000000
 #line = 'abcde' #Test
 
-line = list(line)
-
 time_mp1 = time.time()
+
+line = list(line)
 
 for l in input:
     if l[0] == "p":
@@ -48,10 +49,61 @@ print("Part 1: " + line)
 
 time_mp2 = time.time()
 
+line = 'abcdefghijklmnop'
+line1 = line
+times = 1000000000
+#line = 'abcde' #Test
+j = 0
 
+
+line = list(line1)
+
+while True:
+    j = j + 1
+    for l in input:
+        if l[0] == "p":
+            m = re.match(r'[p](\w+)[/](\w+)', l)
+            n = line.index(m.group(1))
+            o = line.index(m.group(2))
+            line[n], line[o] = line[o], line[n]
+        if l[0] == "s":
+             m = re.match(r'[s](\w+)', l)
+             n = len(line) - int(m.group(1))
+             line = line[n:] + line[0:n]
+        elif l[0] == "x":
+            m = re.match(r'[x](\w+)[/](\w+)', l)
+            n = int(m.group(1))
+            o = int(m.group(2))
+            line[n], line[o] = line[o], line[n]
+
+    if ''.join(line) == line1:
+        times = times % j
+        break
+
+line = list(line1)
+
+for i in range(times):
+    for l in input:
+        if l[0] == "p":
+            m = re.match(r'[p](\w+)[/](\w+)', l)
+            n = line.index(m.group(1))
+            o = line.index(m.group(2))
+            line[n], line[o] = line[o], line[n]
+        if l[0] == "s":
+             m = re.match(r'[s](\w+)', l)
+             n = len(line) - int(m.group(1))
+             line = line[n:] + line[0:n]
+        elif l[0] == "x":
+            m = re.match(r'[x](\w+)[/](\w+)', l)
+            n = int(m.group(1))
+            o = int(m.group(2))
+            line[n], line[o] = line[o], line[n]
+
+line = ''.join(line)
+print("Part 2: " + line)
 
 time_mp3 = time.time()
 
-print("Part 1: %.3f" % (time_mp2 - time_mp1))
+print("Part 2: %.3f" % (time_mp2 - time_mp1))
 print("Part 2: %.3f" % (time_mp3 - time_mp2))
 print("Total:  %.3f" % (time.time() - time_mp0))
