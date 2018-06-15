20cce981c378fbf1c28699d9c4b883a2882f8fab
Gerben van Rijswijk <gvrijswijk@gmail.com>
2018-06-15 21:06:41 +0200

    Reworked first puzzle and solved second

--- 2015/02_i_was_told_there_would_be_no_math.py
+++ 2015/02_i_was_told_there_would_be_no_math.py
@@ -3,18 +3,18 @@
 import re
 
 paper = 0
+ribbon = 0
 
 with open('02_input.txt') as f:
     input = f.readlines()
 
 for line in input:
-    dim = []
-    dims = re.match(r'(\d+)[x](\d+)[x](\d+)', line)
-    dim.append(int(dims[1]) * int(dims[2]))
-    dim.append(int(dims[2]) * int(dims[3]))
-    dim.append(int(dims[1]) * int(dims[3]))
-    for item in dim:
-        paper += 2 * item
-    paper += min(dim)
+
+    dims = list(map(int, re.findall(r'(\d+)', line)))
+    dims.sort()
+
+    paper += 3 * dims[0] * dims[1] + 2 * dims[0] * dims[2] + 2 * dims[1] * dims[2]
+    ribbon += 2 * (dims[0] + dims[1]) + dims[0] * dims[1] * dims[2]
 
 print(paper)
+print(ribbon)
